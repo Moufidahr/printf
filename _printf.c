1f1fc3c58024d9b713a69d727f62750e3d1a02f4
Mina <mina.m.kamal@gmail.com>
2023-07-28 13:56:09 +0300

    redone task with the help of chatgpt

--- _printf.c
+++ _printf.c
@@ -1,51 +1,57 @@
 #include "main.h"
 
-/*
- * _printf function takes a format string
+/**
+ * _printf- Entry point
+ *
+ * Description: 'function takes a format string
  * and a variable number of arguments,
  * and prints the arguments to the standard output
  * according to the format string.
  * It supports the `%c` and `%d` format specifiers
  * for printing characters and integers,
  * respectively. It returns the number of characters
- * printed to the standard output
+ * printed to the standard output'
+ *
+ * @format: parameter
+ *
+ * Return: Always 0 (Success)
  */
 
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int count = 0;
+	va_list args;
+	int count = 0;
 
-        va_start(args, format);
+	va_start(args, format);
 
-        for (const char *p = format; *p; p++)
-        {
-                if (*p != '%')
-                {
-                        write(1, p, 1);
-                        count++;
-                        continue;
-                }
+	for (const char *p = format; *p; p++)
+	{
+		if (*p != '%')
+		{
+			write(1, p, 1);
+			count++;
+			continue;
+		}
 
-                char c = *++p;
+		char c = *++p;
 
-                if (c == 'c')
-                {
-                        print_char(&args);
-                        count++;
-                }
-                else if (c == 'd')
-                {
-                        print_int(&args);
-                        count++;
-                }
-                else
-                {
-                        write(1, p, 1);
-                        count++;
-                }
-        }
+		if (c == 'c')
+		{
+			print_char(&args);
+			count++;
+		}
+		else if (c == 'd')
+		{
+			print_int(&args);
+			count++;
+		}
+		else
+		{
+			write(1, p, 1);
+			count++;
+		}
+	}
 
-        va_end(args);
-        return (count);
+	va_end(args);
+	return (count);
 }
--- functions.c
+++ functions.c
@@ -1,46 +1,63 @@
 #include "main.h"
 
-/*
- * print_char function takes a pointer to a `va_list` as an argument and
- * prints the next argument in the list as a character to the standard output.
+/**
+ * print_char - Entry point
+ *
+ * Description: 'function takes a pointer to a `va_list` as an argument
+ * prints the next argument in the list as a character to the standard output.'
+ *
+ * @args: parameter
+ *
+ * Return: Always 0 (Success)
  */
 void print_char(va_list *args)
 {
-        int ch = va_arg(*args, int);
+	int ch = va_arg(*args, int);
 
-        write(1, &ch, 1);
+	write(1, &ch, 1);
 }
 
-/*
- * int_to_str converts ineger to string and store in array
- * using strlen to length of string
+/**
+ * int_to_str - Entry point
+ *
+ * Description: 'converts ineger to string and store in array
+ * using strlen to length of string'
+ *
+ * @s: parameter
+ * @n: parameter
+ *
+ * Return: Always 0 (Success)
  */
 void int_to_str(int n, char *s)
 {
-    int i = 0;
-    int sign = n;
+	int i = 0;
+	int sign = n;
 
-    if (sign < 0)
-        n = -n;
-    do {
-        s[i++] = n % 10 + '0';
-    } while ((n /= 10) > 0);
-    if (sign < 0)
-        s[i++] = '-';
-    s[i] = '\0';
+	if (sign < 0)
+		n = -n;
+	do {
+		s[i++] = n % 10 + '0';
+	} while ((n /= 10) > 0);
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
 }
 
-/*
- * print_int function takes a pointer to a `va_list` as an argument and
- * prints the next argument in the list as an integer to the standard output.
+/**
+ * print_int - Entry point
+ *
+ * Description: 'function takes a pointer to a va_list as an argument and
+ * prints the next argument in the list as an integer to the standard output.'
+ *
+ * @args: parameter
  */
 void print_int(va_list *args)
 {
-        int i = va_arg(*args, int);
-        int len;
-        char s[20];
+	int i = va_arg(*args, int);
+	int len;
+	char s[20];
 
-        int_to_str(i, s);
-        len = strlen(s);
-        write(1, s, len);
-}}
+	int_to_str(i, s);
+	len = strlen(s);
+	write(1, s, len);
+}
